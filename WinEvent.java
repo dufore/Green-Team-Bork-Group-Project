6d7731a95d4729ffd37ef59f3ba9b20b707634b9
dufore <amosdufore@gmail.com>
2017-04-05 18:51:22 -0400

    Added WinEvent

---
+++ WinEvent.java
@@ -0,0 +1,21 @@
+/*This is a class of type Event. It is triggered when score exceeds winning score value or the main objective is completed
+ *
+ * @author Amos Dufore
+ * @version BorkGroupV1
+ */
+class WinEvent extends Event{
+
+	/*Constructor for WinEvent
+	 */
+	WinEvent(){
+	}
+
+	/*Called when the Win condition is reached
+	 *Congratulation message printed
+	 *asks if you wish to continue
+	 *
+	 * @return String returns continuation/gamefinished message
+	 */
+	String onTrigger(){
+	}
+}
