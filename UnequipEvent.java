6ce73463f49802306b7c99f3a8393ce2a73d75a2
dufore <amosdufore@gmail.com>
2017-04-05 14:26:32 -0400

    Added UnequipEvent.java

---
+++ UnequipEvent.java
@@ -0,0 +1,22 @@
+class UnequipEvent{
+	
+	/*
+	 * Constructor for UnequipEvent
+	 *
+	 * @param String item the name of the item to attempt to unequip
+	 */
+	UnequipEvent(String item){
+	}
+
+	/*
+	 * Called when the UnequipItem event is triggered
+	 * Checks for item in equipped
+	 * 	removes item from equipped
+	 * 	updates stats
+	 *
+	 *@return String returns the string of success/failure
+	 */
+	String onTrigger(){
+	}
+
+}
