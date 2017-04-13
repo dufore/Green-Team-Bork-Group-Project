a000eee4fcd289245949b102499186c7ca00894d
Austin Portell <aportell@mail.umw.edu>
2017-04-13 15:58:12 -0400

    WinEvent

--- WinEvent.java
+++ WinEvent.java
@@ -1,26 +1,28 @@
 package zeitz_borkv3;
+
 /*This is a class of type Event. It is triggered when score exceeds winning score value or the main objective is completed
  *
  * @author Amos Dufore
  * @version BorkGroupV1
  */
-class WinEvent extends Event{
+class WinEvent extends Event {
 
-	/*Constructor for WinEvent
-	 */
-	WinEvent(String w){
-            onTrigger();
+    /*Constructor for WinEvent
+     */
+    WinEvent(String w) {
+        if (w.equals(onTrigger())) {
             System.exit(0);
-	}
+        }
+    }
 
-	/*Called when the Win condition is reached
+    /*Called when the Win condition is reached
 	 *Congratulation message printed
 	 *asks if you wish to continue
 	 *
 	 * @return String returns continuation/gamefinished message
-	 */
-	String onTrigger(){
-            String won = "Congratulations, you have won Bork!";
-            return won;
-	}
+     */
+    String onTrigger() {
+        String won = "Congratulations, you have won Bork!";
+        return won;
+    }
 }
