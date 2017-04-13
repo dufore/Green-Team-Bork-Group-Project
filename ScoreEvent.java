5f8268a23006348f2fadded602ddf2f78368f4f2
Austin Portell <aportell@mail.umw.edu>
2017-04-13 15:58:53 -0400

    ScoreEvent

--- ScoreEvent.java
+++ ScoreEvent.java
@@ -1,4 +1,5 @@
 package zeitz_borkv3;
+
 /*
  * This is an Event class which is triggered when a score is achieved
  *
@@ -6,29 +7,28 @@ package zeitz_borkv3;
  * @version BorkGroupV1
  */
 
-class ScoreEvent extends Event{
-	private int score;
+class ScoreEvent extends Event {
 
+    private int score;
 
-	/*
+    /*
 	 * Constructor for ScoreEvent
 	 *
 	 * @param int score takes the amount to be added to the total score
-	 */
-	ScoreEvent(int score){
-            this.score = score;
-	}
+     */
+    ScoreEvent(int score) {
+        this.score = score;
+    }
 
-	/* Called when the event is triggered
+    /* Called when the event is triggered
 	 * adds amount to total score
 	 *
 	 * @return String returns a string containing a score message "Your score increased by 5"
-	 */
-	String onTrigger(){
-            String scoredString;
-            GameState.instance().addScore(score);
-            scoredString = "Your score increased by " + score;
-            return scoredString;
-	}
+     */
+    String onTrigger() {
+        String scoredString;
+        GameState.instance().addScore(score);
+        scoredString = "Your score increased by " + score;
+        return scoredString;
+    }
 }
-
