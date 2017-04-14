811e7278952d8c0d41e30a0388f8861a840b1776
Austin Portell <aportell@mail.umw.edu>
2017-04-14 17:23:13 -0400

    Comment out NPC from DieEvent

--- DieEvent.java
+++ DieEvent.java
@@ -7,21 +7,21 @@ package zeitz_borkv3;
  */
 class DieEvent extends Event {
 
-    private NonPlayerChar target;
+   // private NonPlayerChar target;
 
     /*Constructor for DieEvent
 	 *
 	 * @param t target (null for player)
      */
-    DieEvent(NonPlayerChar t) {
-        String died = onTrigger();
-        if (died.equals("You have died! Game Over!")) {
-            System.exit(0);
-        } else {
-            GameState.instance().getAdventurersCurrentRoom().getNPCS().remove(t);
-            GameState.instance().getDungeon().getNPCS().remove(t);
-        }
-    }
+   // DieEvent(NonPlayerChar t) {
+     //   String died = onTrigger();
+       // if (died.equals("You have died! Game Over!")) {
+         //  System.exit(0);
+       // } else {
+         //   GameState.instance().getAdventurersCurrentRoom().getNPCS().remove(t);
+          //  GameState.instance().getDungeon().getNPCS().remove(t);
+        //}
+    //}
 
     /*Called when the event is triggered
 	 * Remove NPC from room
