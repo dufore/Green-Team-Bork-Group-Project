7bb16fc8014370c2a1540e899361b765b0fb8c8a
Austin Portell <aportell@mail.umw.edu>
2017-04-14 11:50:45 -0400

    DisappearEvent

--- DisappearEvent.java
+++ DisappearEvent.java
@@ -1,27 +1,40 @@
 package zeitz_borkv3;
+
 /*Event type class called when an item is removed from the game
  *
  * @author Amos Dufore
  * @version BorkGroupV1
  */
-class DisappearEvent extends Event{
-	private String target;
-	/*Constructor for DisappearEvent
+class DisappearEvent extends Event {
+
+    private String target;
+
+    /*Constructor for DisappearEvent
 	 *
 	 * @param t is the target item which is disappearing
-	 */
-	DisappearEvent(String name){
-            this.target = name;
-	}
+     */
+    DisappearEvent(String name) {
+        this.target = name;
+    }
 
-	/*Called when an action is perfromed which requires an item to be removed from gameplay
+    /*Called when an action is perfromed which requires an item to be removed from gameplay
 	 * finds item 
 	 * removes item
 	 *
 	 * returns String informing of item removal
-	 */
-	String onTrigger(){
-            
-            return null;
-	}
+     */
+    String onTrigger() {
+        Item itemToRemove;
+        String disappearString = "";
+
+        try {
+            itemToRemove = GameState.instance().getItemFromInventoryNamed(target);
+            GameState.instance().removeFromInventory(itemToRemove);
+            GameState.instance().getAdventurersCurrentRoom().remove(itemToRemove);
+            disappearString = "The " + target + " has disappeared!";
+        } catch (Item.NoItemException e) {
+
+        }
+        return disappearString;
+    }
 }
